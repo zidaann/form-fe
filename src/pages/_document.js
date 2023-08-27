419d3e4e0a3e5a88c1913ec273cd7183f8607754
zidaann <jourjizaidan8@gmail.com>
2023-08-27 22:11:57 +0700

    refactor: optimize performance and remove aos

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Homepage | Pre Test</title>
+        <meta name="description" content="Pre test membuat form register dan login menggunakna teknologi Next Js dan Tailwind CSS"></meta>
+        <meta name="robots" content="index, follow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charset="UTF-8" />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,10 +1,12 @@
-import FormLogin from '@/components/Fragments/FormLogin'
-import AuthLayouts from '@/components/Layouts/AuthLayouts'
 import React from 'react'
+import FormLogin from '@/components/Fragments/FormLogin'
+import MetaHead from '@/components/Fragments/MetaHead'
+import AuthLayouts from '@/components/Layouts/AuthLayouts'
 
 export default function index() {
   return (
     <>
+      <MetaHead title={'Login'} description={'Login page with username and password'} />
       <AuthLayouts title={'Login'} type={'login'}>
         <FormLogin />
       </AuthLayouts>
